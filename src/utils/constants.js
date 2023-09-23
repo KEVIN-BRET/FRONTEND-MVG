c70d71752b3f5be9143359545d968feee94b91a1
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-09-24 01:29:53 +0200

    https

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 // const API_URL = 'http://localhost:4000';
-const API_URL = 'http://apimvg.kevinbret.fr';
+const API_URL = 'https://apimvg.kevinbret.fr';
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/api/auth/signup`,
   SIGN_IN: `${API_URL}/api/auth/login`,
